555b23641006ad55d2a02e686220845a1a2cf2a6
Miłosz Roliński <rolinski.m@gmail.com>
2018-04-17 18:25:41 +0200

    Added the StatusReader for reading files, and a view to show it.

--- build.sbt
+++ build.sbt
@@ -12,6 +12,7 @@ resolvers += Classpaths.typesafeReleases
 
 libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % ScalatraVersion,
+  "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
   "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "9.4.8.v20171121" % "container",
--- src/main/scala/com/github/cookieburra/hcwebui/hc_webui.scala
+++ src/main/scala/com/github/cookieburra/hcwebui/hc_webui.scala
@@ -1,11 +1,39 @@
 package com.github.cookieburra.hcwebui
 
 import org.scalatra._
+import scalate.ScalateSupport
+
+import scala.io
+import java.io.File
+
+class StatusReader(path : String) {
+  var hostStatus : Map[String,String]= Map()
+
+  try {
+    val hosts : Array[String] = new File(path).list()
+
+    for (host <- hosts) {
+      val handle = io.Source.fromFile(path ++ host)
+
+      hostStatus +=  (host.substring(0, host.lastIndexOf('.')) -> handle.getLines.toArray.mkString(" ").trim)
+      handle.close
+    }   
+  }
+  catch {
+    case e: Exception => {
+      println("Status directory does not exist!")
+      hostStatus = Map("STATUS-DIRECTORY-MISSING" -> "...and that's not good :(")
+    }   
+  }
+}
 
 class hc_webui extends ScalatraServlet {
 
   get("/") {
-    views.html.hello()
+    contentType= "text/html"
+
+		val statusHistories = new StatusReader("./statusfiles/")
+		scaml("index", "statusMap" -> statusHistories.hostStatus)
   }
 
 }
